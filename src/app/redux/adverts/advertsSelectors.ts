bb497550c54238cf9366428359554f9496ba18fb
zim89 <121241841+zim89@users.noreply.github.com>
2023-11-05 17:11:38 +0200

    update redux

--- src/app/redux/adverts/advertsSelectors.ts
+++ src/app/redux/adverts/advertsSelectors.ts
@@ -1,37 +1,37 @@
-import { selectFilter } from '../filter/filterSlice';
 import { RootState } from '../store';
-import { Advert } from './advertsSlice';
+// import { selectFilter } from '../filter/filterSlice';
+// import { Advert } from './advertsSlice';
 
 export const selectIsLoading = (state: RootState) => state.adverts.isLoading;
 export const selectError = (state: RootState) => state.adverts.error;
 export const selectAdverts = (state: RootState) => state.adverts.adverts;
 
-export const selectFilteredAdverts = (state: RootState) => {
-  const { filter } = selectFilter(state);
-  const adverts = state.adverts.adverts;
+// export const selectFilteredAdverts = (state: RootState) => {
+//   const { filter } = selectFilter(state);
+//   const adverts = state.adverts.adverts;
 
-  let res = [...adverts];
+//   let res = [...adverts];
 
-  if (!filter) {
-    return state.adverts.adverts;
-  }
+//   if (!filter) {
+//     return state.adverts.adverts;
+//   }
 
-  Object.entries(filter).forEach(([key, value]) => {
-    if (key === 'make' || key == 'rentalPrice') {
-      res = res.filter(
-        (item) =>
-          String(item[key as keyof Advert]).toLowerCase() ===
-          String(value).toLowerCase(),
-      );
-    }
+//   Object.entries(filter).forEach(([key, value]) => {
+//     if (key === 'make' || key == 'rentalPrice') {
+//       res = res.filter(
+//         (item) =>
+//           String(item[key as keyof Advert]).toLowerCase() ===
+//           String(value).toLowerCase(),
+//       );
+//     }
 
-    if (key === 'mileageTo') {
-      res = res.filter((item) => item.mileage < Number(value));
-    }
+//     if (key === 'mileageTo') {
+//       res = res.filter((item) => item.mileage < Number(value));
+//     }
 
-    if (key === 'mileageFrom') {
-      res = res.filter((item) => item.mileage > Number(value));
-    }
-  });
-  return res;
-};
+//     if (key === 'mileageFrom') {
+//       res = res.filter((item) => item.mileage > Number(value));
+//     }
+//   });
+//   return res;
+// };
